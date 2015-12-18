9c16bcd175d02f43f7dc2e1e38791bea822d2ef4
Matthew Mckenzie <matthew.mckenzie@rungeict.com>
2015-12-18 14:14:31 +1000

    initial babel app

---
+++ src/tests/index.test.js
@@ -0,0 +1,13 @@
+import expect from "expect";
+import deepFreeze from "deep-freeze";
+
+describe("index test", () => {
+  it("basic test", () => {
+    const input = "test";
+    deepFreeze(input);
+    const after = "test1";
+    deepFreeze(after);
+    const result = `${input}1`;
+    expect(after).toEqual(result);
+  });
+});
